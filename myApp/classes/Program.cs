37337cf4864b97540d3286222fe2c81b32485e40
HelenlBlack <helenlblack@outlook.com>
2019-05-25 08:45:31 +0100

    First Upload of files
    Test using VS with github

---
+++ myApp/classes/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using classes;
+
+namespace myApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("BankAccountHLB", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+    }
+}
